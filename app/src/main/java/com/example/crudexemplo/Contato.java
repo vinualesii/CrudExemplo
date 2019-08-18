640bdaa3791224a275af54e273e824c7e334e6cc
vinuales <vinuales@gmail.com>
2019-08-18 10:55:18 -0300

    Thank you

---
+++ app/src/main/java/com/example/crudexemplo/Contato.java
@@ -0,0 +1,60 @@
+package com.example.crudexemplo;
+
+public class Contato {
+
+    private int id;
+    private String fullname;
+    private String username;
+    private String password;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
